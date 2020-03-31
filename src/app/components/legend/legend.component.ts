cb327769816c16b4080cffa2670713a775dc9d07
Raymond Gigliotti <ray@firestitch.com>
2020-03-31 11:31:45 -0400

    added circle shape

--- src/app/components/legend/legend.component.ts
+++ src/app/components/legend/legend.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, HostBinding, OnChanges, SimpleChanges } from '@angular/core';
+
 
 @Component({
   selector: 'fs-legend',
   template: '<ng-content></ng-content>',
   styleUrls: [ 'legend.component.scss' ],
 })
-export class FsLegendComponent {
+export class FsLegendComponent implements OnChanges {
+
+  @HostBinding('class.shape-square') classSquare = true;
+  @HostBinding('class.shape-circle') classCircle = false;
+
+  @Input() shape: 'square' | 'circle' = 'square';
 
   constructor() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.shape) {
+      this.classSquare = this.shape === 'square';
+      this.classCircle = this.shape === 'circle';
+    }
+  }
 }
